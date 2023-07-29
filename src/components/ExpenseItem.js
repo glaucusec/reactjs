59dd5a3f45d55ecfae24f0aaeb751400bf97f9ec
Abhishek <abhishekbaiju@protonmail.com>
2023-07-29 13:02:55 +0530

    Passing Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
-function ExpenseItem() {
-    return (
-        <div>
-            <h2>Expense Item!</h2>
-            <h2>Food Rs 10</h2>
-            <h2>Petrol Rs 100</h2>
-            <h2>Movies Rs 200</h2>
-        </div>
-    )
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
