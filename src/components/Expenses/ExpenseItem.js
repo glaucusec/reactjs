f41742065711f9fe0960f04a8838548c14e817c8
Abhishek <abhishekbaiju@protonmail.com>
2023-08-01 16:59:18 +0530

    PlaceHolder - Adding a filter which doesnt work on the expense tracker

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  let [price, setAmount] = useState(props.amount);
-
-  const titleUpdater = () => {
-    setTitle("Updated");
-  };
-
-  const priceUpdater = () => {
-    price += 100;
-    setAmount(price);
-  };
-
   return (
     <div className="expense">
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
-        <ExpenseDetails title={title} amount={price} />
-        <button onClick={titleUpdater}>Change Title</button>
-        <button onClick={priceUpdater}>Change Amount</button>
+        <ExpenseDetails title={props.title} amount={props.amount} />
       </Card>
     </div>
   );
