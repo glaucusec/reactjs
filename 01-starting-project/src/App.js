738c2ba9e24e40c04acfe85ae196a646ea415d6c
Abhishek <abhishekbaiju@protonmail.com>
2023-08-07 11:51:57 +0530

    Refs and Controlled Components

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+  const [usersList, setUsersList] = useState([
+    { name: "Abhishek", age: 21, college: "NIMIT", id: 1001 },
+  ]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, uCollege) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        {
+          name: uName,
+          age: uAge,
+          college: uCollege,
+          id: Math.random().toString(),
+        },
       ];
     });
   };
--- 01-starting-project/src/components/Users/UsersList.js
+++ 01-starting-project/src/components/Users/UsersList.js
@@ -4,12 +4,13 @@ import Card from '../UI/Card';
 import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
+  console.log(props);
   return (
     <Card className={classes.users}>
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
-            {user.name} ({user.age} years old)
+            {user.name} ({user.age} years old) (and from {user.college})
           </li>
         ))}
       </ul>
